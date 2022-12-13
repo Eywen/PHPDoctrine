7e0c15952aa3df879dc4076701c01c0b135c6dda
Kara <blk20100@gmail.com>
2022-12-13 12:20:45 +0100

    Added users list and links to handle them, handle not work. #8

--- src/controllers.php
+++ src/controllers.php
@@ -9,6 +9,7 @@
 
 use MiW\Results\Entity\User;
 use MiW\Results\Utility\DoctrineConnector;
+use MiW\Results\UserView;
 
 function funcionHomePage(): void
 {
@@ -28,29 +29,107 @@ function funcionListadoUsuarios(): void
 
     $userRepository = $entityManager->getRepository(User::class);
     $users = $userRepository->findAll();
-    var_dump($users);
+    //var_dump($users);
+    vistaListUSer($users);
 }
 
 function funcionUsuario(string $name): void
 {
-    echo $name;
+    echo $name." estoy en funcion usuario";
 }
 
-function funcionNewUsuario(): void
+function crearUsuario(){
+
+    echo " en crear usuario. Falta persisitir";
+
+}
+
+function list_action()
 {
-    //global $routes;
-
-    //$rutaListado = $routes->get('ruta_user_list')->getPath();
-
+    //$posts = "Blanca";
+    global $routes;
+    global $posts;
+    //$posts = $routes->get('ruta_user_list')->getPath();
     $entityManager = DoctrineConnector::getEntityManager();
 
     $userRepository = $entityManager->getRepository(User::class);
-    $users = $userRepository->findBy();
-    var_dump($users);
+    $posts = $userRepository->findAll();
+    //var_dump($posts);
+    //require 'templates/list.php';
 
-//    echo <<< MARCA_FIN
-//    <ul>
-//        <li><a href="$users">Listado Usuarios manual</a></li>
-//    </ul>
-//    MARCA_FIN;
+    //UserView::vista($posts);
+    vistaListUSer($posts);
+}
+
+///////////////////////////  VISTAS  //////////////////////////////
+
+function vistaListUSer($users): void
+{
+    echo "<table>
+            <tr>
+                <th><label>Nombre de usuario</label></th>
+                <th><label>Email</label></th>
+                <th><label>Habilitado</label></th>
+                <th><label>Es Admin</label></th>
+                <th colspan='3'><label>Operaciones</label></th>
+            </tr>";
+    foreach ($users as $user) {
+        $username = $user->getUsername();
+        $email = $user->getEmail();
+        $enabled = $user->isEnabled();
+        $isAdmin = $user->isAdmin();
+        $url = '/users/' . urlencode($username);
+
+        echo <<< ____MARCA_FIN
+                <tr>
+                    <td><label>$username</label></td>
+                    <td><label>$email</label></td> 
+                    <td><label>$enabled</label></td> 
+                    <td><label>$isAdmin</label></td> 
+                    <td><a href="$url">Ver Detalle</a></td>
+                    <td><a href="">Eliminar</a></td>
+                    <td><a href="">Modificar</a></td>
+                </tr>
+    ____MARCA_FIN;
+    }
+
+    global $routes;
+    $rutaNewUSerForm = $routes->get('ruta_user_form')->getPath();
+    echo "<a href=$rutaNewUSerForm>Insertar nuevo usuario prueba con rutas href</a>";
+
+}
+
+
+function testfun()
+{
+    echo "Your test function on button click is working";
+}
+
+
+
+function vistaNewUser()
+{
+    global $routes;
+    $ruta_user_new = $routes->get('ruta_user_new')->getPath();
+
+    echo <<< ____MARCA_FIN
+    
+    <form method="post" action="$ruta_user_new">
+        <h2>Creación de usuario</h2>
+        <table>
+            <tr><td><label>Nombre de usuario: </label></td> 
+                <td><input type="text" name="username" required></td></tr>
+            <tr><td><label>Email: </label></td> 
+                <td><input type="email" name="email" required></td></tr>
+            <tr><td><label>Contraseña: </label></td>
+                <td><input type="password" name="password" required></td></tr>
+            <tr><td><label>Habilitado: </label></td> 
+                <td><input type="checkbox" name="enabled" checked></td></tr>
+            <tr><td><label>Es Admin: </label></td> 
+                <td><input type="checkbox" name="isAdmin"></td></tr>
+            <tr><td colspan="2"><button type="submit">Crear</button></td></tr>
+        </table>
+    </form>
+    ____MARCA_FIN;
+
 }
